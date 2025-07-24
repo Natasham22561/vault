973e6c9d6cabdcffc16af4739a6ff9dc02c6ff4f
Natasha McKenzie <your_email@example.com>
2025-07-23 20:00:30 -0400

    the secret vault

---
+++ index.js
@@ -0,0 +1,32 @@
+let code1 = 'hello world'
+// console.log(code1);
+
+code1 = 7 + 3;
+// console.log(code1);
+
+let code2 = 'hello world again';
+// console.log = (code2);
+
+code2 = 200 / 2 - 60;
+// console.log(code2)
+
+let code3 = 'hello world again happy'
+// console.log(code3)
+
+code3 = (2 % 2) + 39;
+// console.log(code3)
+
+const message = 'The vault is secured. The combination is; ';
+// console.log(message);
+
+const codeA = code1 + code2 + code3;
+// console.log(codeA);
+
+const codeB = (code1)-$ (code2) -$(code3);
+//console.log(codeB);
+
+console.log(`${message} ${codeA} ${codeB} `)
+
+
+
+
